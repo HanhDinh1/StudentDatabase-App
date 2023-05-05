c026840f3f9bdc853e5e75fa08ca925f5b5dac13
HanhDinh1 <ms.myhanh89@gmail.com>
2023-05-05 07:04:04 -0400

    student ID

--- src/studentdatabaseapp/Student.java
+++ src/studentdatabaseapp/Student.java
@@ -6,10 +6,11 @@ public class Student {
 	private String firstName;
 	private String lastName;
 	private int gradeYear;
-	private int studentID;
+	private String studentID;
 	private String courses;
 	private int tuitionBalance;
-	private int costOfCourse = 600;
+	private static int costOfCourse = 600;
+	private static int id = 1000;
 
 	// Constructor: prompt user to enter student's name and year
 	public Student() {
@@ -22,10 +23,19 @@ public class Student {
 		
 		System.out.println("1 - Freshman\n2 - Sophmore\n3 - Jumior\n4 - Junior\nEnter student grade level: ");
 		this.gradeYear = in.nextInt();
-		System.out.println(firstName + " " + lastName + " " + gradeYear);
+		
+		setStudentID();
+		
+		System.out.println(firstName + " " + lastName + " " + gradeYear + " " + studentID);
+		
 	}
 	
 	// Generate an ID
+	private void setStudentID() {
+		// Grade Level + ID
+		id++;
+		this.studentID = (gradeYear + "" + id);
+	}
 	
 	// Enroll in courses
 	
